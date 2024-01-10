e56a6acd64cae0a16f296f746d09ffacc4a89226
HasanEren72 <101269156+HasanEren72@users.noreply.github.com>
2024-01-10 20:22:58 +0300

    update

--- OrientationControl.cs
+++ OrientationControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YonKontrol : MonoBehaviour
+public class OrientationControl: MonoBehaviour
 {
     [SerializeField]
     private ScreenOrientation defaultYon = ScreenOrientation.Portrait;
